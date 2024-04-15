c6b692abcfe2f3c3830a2814941e80638b975ea7
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-15 15:05:39 +0100

    sixth

--- src/main/java/org/example/gui/MyAppController.java
+++ src/main/java/org/example/gui/MyAppController.java
@@ -2,13 +2,32 @@ package org.example.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MyAppController {
     @FXML
-    private Label subbutton;
+    private Label firstname;
+    @FXML
+    private TextField firstnametext;
+    @FXML
+    private Label lastname;
+    @FXML
+    private TextField lastnametext;
+    @FXML
+    private Label email;
+    @FXML
+    private TextField emailtext;
+    @FXML
+    private Label course;
+    @FXML
+    private TextField coursetext;
 
     @FXML
     protected void ButtonClick() {
-        subbutton.setText("INPUT SUCESSFUL!");
+        firstname.setText(firstname.getText()+" "+firstnametext.getText());
+        lastname.setText(lastname.getText()+" "+lastnametext.getText());
+        email.setText(email.getText()+" "+emailtext.getText());
+        course.setText(course.getText()+" "+coursetext.getText());
+
     }
 }
